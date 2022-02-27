e8f22920e24afbe9dbf987359058eaebc67ca039
HuVik <vhuber9007@gmail.com>
2022-02-27 18:31:04 +0100

    fixedpwd
    I fixed the createpwd method and it's html page.

--- sb_squash_mvc/src/main/java/pti/sb_squash_mvc/controller/AppController.java
+++ sb_squash_mvc/src/main/java/pti/sb_squash_mvc/controller/AppController.java
@@ -18,7 +18,10 @@ public class AppController {
 		return "login";
 	}
 	
-
+	@GetMapping("createpwd")
+	public String createNewPwd() {
+		return "createnewpwd";
+	}
 	
 	@PostMapping("/welcomepage")
 	public String loginByAdmin(Model model, 
@@ -84,23 +87,34 @@ public class AppController {
 		return "admin";
 	}
 	
-	@PostMapping("/changepassword")
+	@PostMapping("/showchangepassword")
 	public String changePassword(Model model, 
 			@RequestParam(name = "username") String userName,
+			@RequestParam(name = "oldpwd") String oldPwd,
 			@RequestParam(name="userspassword") String usersPwd,
 			@RequestParam(name="newuserspassword") String newUsersPwd) {
 		
 			Database db = new Database();
 			
-			if((!userName.isEmpty()) && (!usersPwd.isEmpty()) && (!newUsersPwd.isEmpty())) {
-				User user = db.getUserByNameAndPwd(userName, usersPwd);
-				if((user != null) && (!usersPwd.equals(newUsersPwd)) && (!user.getPwd().equals(newUsersPwd))) {
-					//save user width his/her new password
-					db.updateUser(user);
-					model.addAttribute("success", "A jelszó sikeresen módosítva!");
+			if((!userName.isEmpty()) && (!oldPwd.isEmpty()) && (!usersPwd.isEmpty()) && (!newUsersPwd.isEmpty())) {
+				User user = db.getUserByNameAndPwd(userName, oldPwd);
+				if(user != null) {
+					if(!user.getPwd().equals(newUsersPwd) && (!user.getPwd().equals(usersPwd))) {
+						//save user width his/her new password
+						user.setId(user.getId());
+						user.setName(userName);
+						user.setPwd(newUsersPwd);
+						user.setRole(user.getRole());
+						user.setEntered(user.isEntered());
+						user.setPieceOfEntry(user.getPieceOfEntry());
+						db.updateUser(user);
+						model.addAttribute("success", "A jelszó sikeresen módosítva!");
+					}else {
+						//password is exists
+						model.addAttribute("warning", "Az új jelszó nem egyezhet meg a régivel!");
+					}
 				}else {
-					//password is exists
-					model.addAttribute("warning", "Az új jelszó nem egyezhet meg a régivel!");
+					model.addAttribute("nofound", "A megadott felhasználó nem létezik a rendszerben!");
 					
 				}
 			}else {
